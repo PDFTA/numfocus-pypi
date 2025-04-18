0b1242cf1e317b65b0d18860be608ac09d817562
John Carney <john.x.carney@johnlewis.co.uk>
2025-04-18 19:49:28 +0100

    Added Simplest model

---
+++ models/bronze_numfocus_projects.sql
@@ -0,0 +1,14 @@
+{{
+    config(
+        labels={"tier": "bronze"},
+        description='NumFOCUS downloads data from the GBQ public dataset.',
+    )
+}}
+WITH source AS (
+    SELECT
+        *,
+    FROM {{ source("pypi", "file_downloads") }}
+    WHERE TIMESTAMP_TRUNC(timestamp, DAY) = TIMESTAMP("2025-04-16")
+        AND file_downloads.file.project IN ('pymc3')
+
+)
