78aea160c58a78eb909461843d73ee021280a84a
John Carney <john.x.carney@johnlewis.co.uk>
2025-08-10 01:36:59 +0100

    Got it working locally

--- models/bronze_numfocus_projects.sql
+++ models/bronze_numfocus_projects.sql
@@ -14,10 +14,14 @@ SELECT
     *,
 FROM {{ source("pypi", "file_downloads") }}
 WHERE file_downloads.file.project IN ('pymc3')
+AND timestamp BETWEEN
+        TIMESTAMP(DATE_SUB(CURRENT_DATE, INTERVAL + 366 DAY))
+         AND TIMESTAMP(DATE_SUB(CURRENT_DATE, INTERVAL +1 DAY))
 
 {% if is_incremental() %}
 
 AND timestamp BETWEEN
-        TIMESTAMP(DATE_SUB(CURRENT_DATE, INTERVAL + 2) DAY) AND TIMESTAMP(DATE_SUB(CURRENT_DATE, INTERVAL +1 DAY))
+        TIMESTAMP(DATE_SUB(CURRENT_DATE, INTERVAL + 2 DAY))
+         AND TIMESTAMP(DATE_SUB(CURRENT_DATE, INTERVAL +1 DAY))
 
 {% endif %}
